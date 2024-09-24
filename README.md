c8de414ef85ec45474b302e5b35496aafc9fd964
SHREY KHATER <shrey@Shreys-MacBook-Pro.local>
2024-09-23 20:19:30 -0400

    finished implementing entropy tests

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # qc-classifier
 Github home for DoraHacks' work regarding AI-inspired classification of quantum computer random number data (QRNG).
 
+The eventual goal is a deployed model that can verify QRNG data with high accuracy to pair with our QRNG API.
+
 Feel free to email shrey@dorahacks.biz if you have any questions.
--- modules/feature_extraction_functions.py
+++ modules/feature_extraction_functions.py
@@ -12,7 +12,7 @@ def shannon_entropy(input_string: str, possible_values: str, chunk_size: int = 1
     Args:
         input_string (string): data, in string format, to extract entropy calculation from (required)
         possible_values (string): every possible value present in the input data (required)
-        chunk_size (int): the size of the chunks to calculate entropy on; must be divisible by size of input data string (optional, defaults to 1)
+        chunk_size (int): the size of the chunks to calculate entropy on; must be divisible by size of input data string and also be <= 12 (optional, defaults to 1)
 
     Returns:
         float: Shannon entropy calculation
@@ -23,7 +23,9 @@ def shannon_entropy(input_string: str, possible_values: str, chunk_size: int = 1
 
     if (input_length % chunk_size) != 0:
         return ValueError('The chunk size must be a factor of the length of the input string; i.e. input_length mod (chunck_size) = 0')
-    
+    if chunk_size > 12:
+        return ValueError('The supplied chunk size must be less than 13 for space constraints')
+
     patterns = [''.join(pattern) for pattern in itertools.product(possible_values, repeat=chunk_size)]
 
     frequencies = {key: 0 for key in patterns}
@@ -43,7 +45,45 @@ def shannon_entropy(input_string: str, possible_values: str, chunk_size: int = 1
 
     return entropy(probabilities, base=2)
     
+
+def shannon_entropy_integer(input_string: str, chunk_size: int = 1):
+    """Calculate Shannon (standard) entropy for a given string, but by converting each chunk to an integer first, avoiding the cartesian product 
+       complexity for all possible bitstrings. Chunk size however must be less than 30 for practicality. Uses scipy implementation
+
+    Args:
+        input_string (string): data, in string format, to extract entropy calculation from (required)
+        possible_values (string): every possible value present in the input data (required)
+        chunk_size (int): the size of the chunks to calculate entropy on; must be divisible by size of input data string and < 30 (optional, defaults to 1)
+
+    Returns:
+        float: Shannon entropy calculation
+
+    """
+
+    input_length = len(input_string)
+
+    if chunk_size > 29:
+        return ValueError('The supplied chunk size must be less than 30 for space constraints')
+    if (input_length % chunk_size) != 0:
+        return ValueError('The chunk size must be a factor of the length of the input string; i.e. input_length mod (chunck_size) = 0')
     
+    #Array size is total possible binary sequences (converted to integers)
+    frequencies = [0] * (2**chunk_size)
+    #iterate though each "chunk"
+    for i in range(0, len(input_string), chunk_size):
+        binarySum = 0
+        for j in range(i, i+chunk_size):
+            if input_string[j] == '1':
+                binarySum += 2 ** (i+chunk_size-j-1)
+        frequencies[binarySum] += 1
+    
+    total_segments = input_length / chunk_size
+    probabilities = []
+    for i in frequencies:
+        probabilities.append(i / total_segments)
+
+    return entropy(probabilities, base=2)
+ 
 
 def runs_test(bit_string):
     n = len(bit_string)
